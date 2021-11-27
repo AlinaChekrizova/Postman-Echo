ad8e993433ba14032b3e05af5040c6991ac900cd
Alina Chekrizova <alinaspleen@gmail.com>
2021-11-27 20:01:59 +0300

    test run

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Lorem ipsum dolor sit ameta"))
+                .body("data", equalTo("Lorem ipsum dolor sit amet"))
         ;
     }
 }
